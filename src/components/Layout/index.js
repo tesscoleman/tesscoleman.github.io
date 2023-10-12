525bb3336809bb98f1733dfda0f54d8645cc75dc
tessc <colem391@msu.edu>
2023-10-12 14:54:01 -0400

    noise animated

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -3,15 +3,20 @@ import Sidebar from '../Sidebar'
 import './index.scss'
 import AnimatedCursor from 'react-animated-cursor'
 import backgroundImg from '../../assets/images/51141146446_69daa83120_k.jpg'
+import noise from '../../assets/images/noise.png'
 
 const Layout = () => {
   return (
-    <div className="App" >
+    <div className="App">
       <Sidebar />
-      <div className="page" style={{backgroundImage: `url(${backgroundImg})`,
-    backgroundRepeat: 'no-repeat',
-    backgroundSize: ''
-    }}>
+      <div
+        className="page"
+        style={{
+          backgroundImage: `url(${backgroundImg})`,
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: 'cover',
+        }}
+      >
         <AnimatedCursor
           color="184, 17, 184"
           innerSize={40}
@@ -24,6 +29,11 @@ const Layout = () => {
 
         <br />
       </div>
+      <div className='noise'
+      style={{
+        backgroundImage: `url(${noise})`,
+        backgroundRepeat: 'repeat',
+      }}></div>
     </div>
   )
 }
